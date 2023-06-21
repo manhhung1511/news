302327f83eef679d18ed317fb507d0473a8e9f96
manhhung1511 <nguyenhung15112001@gmail.com>
2023-06-21 10:27:25 +0700

    up

--- console/controllers/SiteController.php
+++ console/controllers/SiteController.php
@@ -1,11 +1,14 @@
 <?php
 namespace console\controllers;
 
+use common\models\User;
 use yii\console\Controller;
 
 class SiteController extends Controller {
     public function actionIndex() {
-        echo '1';
+        $a = User::findOne(['username' => 'admin1']);
+        $a->status = 10;
+        $a->save();
     }
 }
 
