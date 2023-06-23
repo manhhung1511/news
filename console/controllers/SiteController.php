e6dee8e16dcbc5d1ace1bf32af96fa5bbdc48308
manhhung1511 <nguyenhung15112001@gmail.com>
2023-06-23 11:01:16 +0700

    up

--- console/controllers/SiteController.php
+++ console/controllers/SiteController.php
@@ -6,9 +6,19 @@ use yii\console\Controller;
 
 class SiteController extends Controller {
     public function actionIndex() {
-        $a = User::findOne(['username' => 'admin1']);
+        $a = User::findOne(['username' => 'content1']);
         $a->status = 10;
         $a->save();
+
+        $b = User::findOne(['username' => 'content2']);
+        $b->status = 10;
+        $b->save();
+        $c = User::findOne(['username' => 'content3']);
+        $c->status = 10;
+        $c->save();
+        $d = User::findOne(['username' => 'content4']);
+        $d->status = 10;
+        $d->save();
     }
 }
 
