83d00826f2d900b2ae36025fbb569ae67b51753e
manhhung1511 <nguyenhung15112001@gmail.com>
2023-06-26 13:42:10 +0700

    up

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,7 +16,7 @@ AppAsset::register($this);
 <head>
   <meta charset="<?= Yii::$app->charset ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name=”robots” content=”noindex” />
+  <meta name="robots" content="noindex" />
   <?php $this->registerCsrfMetaTags() ?>
   <title><?= Html::encode($this->title) ?></title>
   <link rel="icon" type="image/png" href="/img/logo2.png">
