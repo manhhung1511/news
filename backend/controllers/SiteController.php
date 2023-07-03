ca6ba0fd965abcecfbec32c9bf0221fae9fe9712
manhhung1511 <nguyenhung15112001@gmail.com>
2023-07-03 16:50:57 +0700

    up

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -89,7 +89,7 @@ class SiteController extends Controller
                 ]),
             'sort' => ['defaultOrder' => ['_id' => SORT_DESC]],
             'pagination' => [
-                'pageSize' => 30
+                'pageSize' => 50
             ]
         ]);
 
