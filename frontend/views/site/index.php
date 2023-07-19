ea030b0333614b62fb32cd303ca1af8edca8c219
manhhung1511 <nguyenhung15112001@gmail.com>
2023-07-19 13:38:59 +0700

    up

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -105,7 +105,7 @@ function create_slug3($string)
                             <div class="jl-roww content-inner jl-col3 jl-col-row">
                                 <div class="jl_sec_title">
                                     <h3 class="jl_title_c">
-                                        <a href="http://frontend-techway1.com/category/benh/<?=$category[0]->slug?>"> 
+                                        <a href="https://songxanh24h.vn/category/benh/<?=$category[0]->slug?>"> 
                                             <span><?= $category[0]->name ?></span>
                                         </a>
                                     </h3>
@@ -145,7 +145,7 @@ function create_slug3($string)
                 <div class="col-md-12">
                     <div class="jl_sec_title">
                         <h3 class="jl_title_c">
-                            <a href="http://frontend-techway1.com/category/benh/<?=$category[1]->slug?>"> 
+                            <a href="https://songxanh24h.vn/category/benh/<?=$category[1]->slug?>"> 
                                 <span><?= $category[1]->name ?></span>
                             </a>
                         </h3>
@@ -198,7 +198,7 @@ function create_slug3($string)
                             <div class="jl-roww content-inner jl-col3 jl-col-row">
                                 <div class="jl_sec_title">
                                     <h3 class="jl_title_c">
-                                        <a href="http://frontend-techway1.com/category/benh/<?=$category[2]->slug?>"> 
+                                        <a href="https://songxanh24h.vn/category/benh/<?=$category[2]->slug?>"> 
                                             <span><?= $category[2]->name ?></span>
                                         </a>
                                     </h3>
