aa340ecc0375539d9949a92486c5171ea22aa52a
manhhung1511 <nguyenhung15112001@gmail.com>
2023-08-08 11:34:19 +0700

    up

--- console/controllers/SiteController.php
+++ console/controllers/SiteController.php
@@ -4,8 +4,11 @@ namespace console\controllers;
 use common\models\Category;
 use common\models\News;
 use Google_Client;
+use Google_Service;
 use Yii;
 use yii\console\Controller;
+use Google\Service\Blogger;
+use Google\Service\Blogger\Post;
 
 class SiteController extends Controller {
     public static function  Slug ($string)
@@ -190,5 +193,87 @@ class SiteController extends Controller {
         //     echo "URL: " . $response['url'] . " | Response: " . json_encode($response['response']) . "<br>";
         // }   
      }   
+
+     public function actionBlogger() {
+    
+        $client = new Google_Client();
+        $client->setApplicationName('Your Application Name');
+        $client->setDeveloperKey('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');
+
+        // Create an instance of the Blogger service
+        $service = new Blogger($client);
+        
+        // Create a new post
+        $blogId = '2638507688997638382';
+        $post = new Post();  
+        $post->setTitle('My New Post');
+        $post->setContent('This is the content of my new post');
+        
+        $createdPost = $service->posts->insert($blogId, $post);
+        
+        // Check if the post was created successfully
+        if ($createdPost) {
+            echo 'Post created successfully';
+        } else {
+            echo 'Failed to create post';
+        }
+     }
+
+     public function actionTest() {
+        $updates = Category::findAll(["status" => 1]);
+        foreach($updates as $item) {
+            $item->status = 2;
+            $item->save();
+        }
+
+        $update = Category::findOne(["_id" => new ObjectId("649916d37a412d2beb6d2ce4")]);
+
+        $update->status = 1;
+        $update->save();
+
+       
+     }
+    //  public function actionTest() {
+    //     // $data = array(
+    //     //     'client_id' => 'your_client_id',
+    //     //     'client_secret' => 'your_client_secret',
+    //     //     'refresh_token' => 'your_refresh_token',
+    //     //     'grant_type' => 'refresh_token',
+    //     // );
+        
+    //     // $options = array(
+    //     //     'http' => array(
+    //     //         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+    //     //         'method'  => 'POST',
+    //     //         'content' => http_build_query($data),
+    //     //     ),
+    //     // );
+        
+    //     // $context  = stream_context_create($options);
+    //     // $result = file_get_contents('https://oauth2.googleapis.com/token', false, $context);
+        
+    //     // if ($result === FALSE) { /* Handle error */ }
+        
+    //     // var_dump(json_decode($result, true));
+    //     $client = new Google_Client();
+    //     // $client->setClientId('850065321278-36vaon2m6hf2bge78op80ld070eu5llu.apps.googleusercontent.com');
+    //     // $client->setClientSecret('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');
+    //     // $client->setRedirectUri('https://songxanh24h.com');
+    //     // $client->addScope('https://www.googleapis.com/auth/blogger');
+    //     // $client->setAccessType('offline'); // This is for getting refresh token
+        
+    //     // if (isset($_GET['code'])) {
+    //         // Authenticate and get the access token
+    //         $token = $client->fetchAccessTokenWithAuthCode('4/0Adeu5BUEGCKqDvgz3Xyw-pNCAskr5TdCnaf24P1FNXlWGE0foQsZWep_E5fCjQhQoPWoJQ');
+    //         // echo 'Access Token: ' . $token['access_token'] . '<br>';
+    //         // echo 'Refresh Token: ' . $token['refresh_token'] . '<br>';
+    //         var_dump($token);
+    //     // } else {
+    //         // echo '1';
+    //         // Generate a URL and redirect the user to it
+    //         // $authUrl = $client->createAuthUrl();
+    //         // header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+    //     // }
+    //  }
 }
 ?>
