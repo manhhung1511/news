fe9528d11ecee2112ae257486653213e31de8926
manhhung1511 <nguyenhung15112001@gmail.com>
2023-09-22 15:57:40 +0700

    up

--- frontend/views/crawl/hospital.php
+++ frontend/views/crawl/hospital.php
@@ -50,7 +50,11 @@ $this->title = Tools::subWord($title, 8);
                                 <a title="" href="<?= Yii::$app->urlManager->createAbsoluteUrl(['crawl/hospital-detail', 'slug' => $item->slug,'slug-category' => $item->slug_category]) ?>"> <?= $item->name ?> </a>
                             </div>
                             <a href="<?= Yii::$app->urlManager->createAbsoluteUrl(['crawl/hospital-detail', 'slug' => $item->slug, 'slug-category' => $item->slug_category]) ?>">
-                                <img src="https://storage.songxanh24h.vn/images/2023/09/15/hospitalwebp.webp" alt="<?= $item->name ?>">
+                                <?php if($item->img != '#'): ?>
+                                    <img src="https://storage.songxanh24h.vn/images/hospital1/<?= $item->img ?>" alt="<?= $item->name ?>">
+                                <?php else: ?>
+                                    <img src="https://storage.songxanh24h.vn/images/2023/09/15/hospitalwebp.webp" alt="<?= $item->name ?>">
+                                <?php endif; ?>
                             </a>
                             <div class="hospital_content">
                                 <div class="hospital_description">
