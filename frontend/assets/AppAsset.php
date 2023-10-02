3b76a35bbf384df51a3fe3320af7ff1656283921
manhhung1511 <nguyenhung15112001@gmail.com>
2023-10-02 10:38:57 +0700

    up

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,8 +12,8 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css'
-        // 'css/site.min.css?v=1.5.0',
+        // 'css/site.css'
+        'css/site.min.css?v=1.6.0',
         // 'css/fonts/jl_font.ttf',
         // 'css/fonts/P5sMzZCDf9_T_10ZxCE.woff2',
         // 'css/fonts/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2'
